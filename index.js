a4173f3272188a7a8e08e66f73ef889061d69eaf
i'mM <min03041999@gmail.com>
2023-07-22 14:01:36 +0700

    feat(source-code): build structure project

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mongoose = require("mongoose"); // store mongodb
+
+const cors = require("cors");
+const cookieParser = require("cookie-parser"); // it is middware and use syntax anlysis
+const fs = require("fs");
+const multer = require("multer");
+
+// Routes
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json()); // return data according to format json
+app.use(cookieParser());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://192.168.18.2",
+  })
+); // allow access address ip 192.168.18.2 source app
+
+const port = 3000;
+
+app.get("/test", (req, res) => {
+  res.json({
+    message: "Data is successful",
+    data: {
+      title: "Xin chào",
+      name: "A Min",
+    },
+  });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://min03041999:min03041999@test.ajpc80k.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    const sever = app.listen(port);
+    console.log(`Server is running in port ${port}`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
