e1ae2b5a9f14f47fab3688e3ccd66f4d82ea1b45
DESKTOP-4GUMU4J\Amin <min03041999@gmail.com>
2023-07-23 17:05:49 +0700

    feat(user): using swaggerui

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose"); // store mongodb
 
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+
 const cors = require("cors");
 const cookieParser = require("cookie-parser"); // it is middware and use syntax anlysis
 const fs = require("fs");
@@ -13,10 +16,94 @@ const app = express();
 app.use(express.json()); // return data according to format json
 app.use(cookieParser());
 
+// Swagger UI
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Node JS API Project for mongodb",
+      version: "1.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/",
+      },
+    ],
+  },
+
+  apis: ["./index.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
 // Routes
 const userRoutes = require("./Routes/user.routes");
 
 app.use("/user", userRoutes);
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Users:
+ *          type: object
+ *          required:
+ *              - name
+ *              - email
+ *              - password
+ *          properties:
+ *              name:
+ *                  type: string
+ *                  description: name is yourname
+ *              email:
+ *                  type: string
+ *                  description: email is unique
+ *              password:
+ *                  type: string
+ *                  description: password is yourpassword
+ *
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: The users managing API
+ * /user/get-user:
+ *   get:
+ *     summary: Lists all the users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: The list of the users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Users'
+ * /user/register:
+ *   post:
+ *     summary: Create a user
+ *     tags: [Users]
+ *     requestBody:
+ *        required: true
+ *        content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Users'
+ *     responses:
+ *       200:
+ *         description: The created user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Users'
+ *       500:
+ *         description: Some server error
+ *
+ */
 
 app.use(
   cors({
